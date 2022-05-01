fc605d49f3b03f8e15e57e6dbbccb9ea0c570841
ThomasJose1999 <thomas.jose@phaethon.tech>
2022-05-01 13:40:33 +0530

    add required

--- kyc.sol
+++ kyc.sol
@@ -8,6 +8,7 @@ contract kyc{
         string name;
         address add;
         bool status;
+        uint exist;
     }
 
     mapping(uint256 => Customer) private idToCustomer;
@@ -18,23 +19,28 @@ contract kyc{
     }
 
     function addRWAO(uint rwaoId,string memory name, address add) public {
+        require(idToCustomer[rwaoId].exist != 1, "rwao id already exist");
         idToCustomer[rwaoId] =  Customer(
         rwaoId,
         name,
         add,
-        false
+        false,
+        1
       );
     }
 
     function checkStatus(uint rwaoId) view public returns (bool){
+        require(idToCustomer[rwaoId].exist == 1, "rwao id does not exist");
         return (idToCustomer[rwaoId].status);
     }
 
     function updateStatus(uint rwaoId, bool _status) public {
+        require(idToCustomer[rwaoId].exist == 1, "rwao id does not exist");
         idToCustomer[rwaoId].status = _status;
     }
 
     function getRwao(uint rwaoId) view public returns(string memory name, address add){
+        require(idToCustomer[rwaoId].exist == 1, "rwao id does not exist");
         return (idToCustomer[rwaoId].name, idToCustomer[rwaoId].add);
     }
 
